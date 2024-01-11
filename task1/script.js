d318473bd4b434af6bab88821258fda09d426375
Sayali Gaikwad <120018636+SayaliG6206@users.noreply.github.com>
2024-01-11 23:01:29 +0530

    Update script.js

--- task1/script.js
+++ task1/script.js
@@ -20,10 +20,6 @@ function Clear(){
 
 function del(){
     outputScreen.value = outputScreen.value.slice(0, -1);
-}/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+}
 
 
